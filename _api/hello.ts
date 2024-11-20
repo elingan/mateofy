2b78398c8a7e51885ce6e068a67314f4e2f82cbe
Eduardo Lingan <elingan@gmail.com>
2024-11-20 20:45:19 +0100

    feat: initialize project structure with Next.js, Tailwind CSS, and Clerk middleware

--- _api/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
- 
-export default async function handler(request: VercelRequest, response: VercelResponse) {
-  const { name = 'World' } = request.query;
-  return response.send(`Hello ${name}!`);
-}
---
+++ src/components/spotify-web.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function SpotifyWeb() {
+
+  useEffect(() => {
+    console.log('Spotify Web')
+  })
+
+  return (
+    <div>
+      <h1>Spotify Web</h1>
+    </div>
+  )
+}

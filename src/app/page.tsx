57f5ce82d98adfc296a064ac1cd2585e4d66a023
Eduardo Lingan <elingan@gmail.com>
2024-11-20 20:57:39 +0100

    feat: remove artificial delay in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { SignedIn, SignedOut, SignIn } from '@clerk/nextjs'
 
 export default async function Home() {
 
-  await new Promise((resolve) => setTimeout(resolve, 1000))
+  // await new Promise((resolve) => setTimeout(resolve, 1000))
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">

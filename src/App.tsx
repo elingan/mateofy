743ecd36de12f1386fea2ffdb370e4666a897051
Eduardo Lingán <elingan@gmail.com>
2024-11-20 13:00:08 +0100

    feat: replace hello API endpoint with Vercel request handling and update package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('/api/hello?name=Vite')
+      const data = await response.text()  
+      console.log(data)
+    })()
+  })
 
   return (
     <>
